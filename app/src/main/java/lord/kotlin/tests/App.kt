7efa38189a14eb9cbf3180404d63f3617e92371a
dan9112_work_KamAZ <bessonovdanil9112@mail.ru>
2021-10-15 09:37:17 +0300

    init

---
+++ app/src/main/java/lord/kotlin/tests/App.kt
@@ -0,0 +1,13 @@
+package lord.kotlin.tests
+
+import android.app.Application
+import timber.log.Timber
+
+class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
