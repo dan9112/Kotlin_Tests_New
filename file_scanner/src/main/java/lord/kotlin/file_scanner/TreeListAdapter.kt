40f186fb36fa48c6978f65118cae50696b39dfcb
dan9112_work_KamAZ <bessonovdanil9112@mail.ru>
2021-10-18 14:56:59 +0300

    v. 2.1
    1. Оптимизировано обновление контента RecyclerView после обработки
    нажатия на непустую директорию, содержимое которой доступно
    приложению для просмотра
    2. Исправлено отображение пустых директорий и директорий, с
    недоступным для просмотра приложением содержимым
    3. Добавлены иконки для файлов картиок, аудио и видео
    4. Добавлено отображение иконки в зависимости от расширения файлов
    (недоступные для просмотра отображаются по-старому)

--- file_scanner/src/main/java/lord/kotlin/file_scanner/TreeListAdapter.kt
+++ file_scanner/src/main/java/lord/kotlin/file_scanner/TreeListAdapter.kt
@@ -1,6 +1,5 @@
 package lord.kotlin.file_scanner
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
@@ -10,17 +9,13 @@ import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import lord.kotlin.file_scanner.databinding.ListItemBinding
-import kotlin.collections.ArrayList
 
-@SuppressLint("NotifyDataSetChanged")
 class TreeListAdapter(
     /** Контекст RecyclerView */
     private val context: Context,
     /** Данные входящего списка */
     private val treeItems: ArrayList<TreeItem>
 ) : RecyclerView.Adapter<TreeListAdapter.TreeViewHolder>() {
-    /** Флаг первой загрузки, чтобы было удобно добавлять данные в список отображения */
-    private var begin: Boolean
 
     /** Отображаемые данные списка */
     private val itemList: MutableList<TreeItem>
@@ -43,12 +38,9 @@ class TreeListAdapter(
 
     // Поскольку этот метод вызывается первым каждый раз при обновлении списка, заполните список itemList в этом методе
     override fun getItemCount(): Int {
-        if (begin) {
-            itemList.clear()
-            getList(treeItems)
-            // Добавляем только один раз за обновление
-            begin = false
-        }
+        itemList.clear()
+        getList(treeItems)
+        // Добавляем только один раз за обновление
         return itemList.size
     }
 
@@ -62,6 +54,15 @@ class TreeListAdapter(
             // передаём список подпунктов этого элемента
             if (it.sons != null && it.isOpen) getList(it.sons!!)
         }
+    }
+
+    private fun getCount(treeItems: List<TreeItem>): Int {
+        var count = 0
+        treeItems.forEach {
+            count++
+            if (it.sons != null && it.isOpen) count += getCount(it.sons!!)
+        }
+        return count
     }
 
     inner class TreeViewHolder(binding: ListItemBinding) : RecyclerView.ViewHolder(binding.root) {
@@ -81,19 +82,42 @@ class TreeListAdapter(
                     )
                     if (sons!![0].string == null) {// Директория, содержимое которой недоступно, либо она пуста
                         itemView.setOnClickListener(null)
-                        textView.setTextColor(ContextCompat.getColor(context, android.R.color.holo_red_dark))
+                        iconView.setColorFilter(
+                            ContextCompat.getColor(
+                                context,
+                                android.R.color.holo_red_dark
+                            )
+                        )
+                        textView.setTextColor(
+                            ContextCompat.getColor(
+                                context,
+                                android.R.color.holo_red_dark
+                            )
+                        )
                     } else {
+                        iconView.setColorFilter(
+                            ContextCompat.getColor(
+                                context,
+                                android.R.color.black
+                            )
+                        )
                         // Добавляем событие щелчка к изображению списка с дочерними элементами, изменяем, расширять ли
                         itemView.setOnClickListener {
                             isOpen = !isOpen
                             // Обновляем список и снова добавляем данные
-                            begin = true
-                            notifyDataSetChanged()
+                            notifyItemChanged(position)
+                            val count = getCount(this.sons!!)
+                            if (isOpen) notifyItemRangeInserted(position + 1, count)
+                            else notifyItemRangeRemoved(position + 1, count)
+                            notifyItemRangeChanged(position + 1, itemList.size - position)
                         }
                     }
                 } else {
                     // Устанавливаем флаг для корневого узла
-                    iconView.setImageResource(R.drawable.ic_launcher_background)
+                    iconView.apply {
+                        setImageResource(getIconResource(string!!.substringAfterLast('.', "")))
+                        clearColorFilter()
+                    }
                     itemView.setOnClickListener(null)
                 }
                 //Добавить текст
@@ -104,14 +128,14 @@ class TreeListAdapter(
 
     init {
         itemList = java.util.ArrayList()
-        begin = true
     }
 
-    @SuppressLint("NotifyDataSetChanged")
-    fun update(newList: ArrayList<TreeItem>) {
-        treeItems.clear()
-        treeItems.addAll(newList)
-        begin = true
-        notifyDataSetChanged()
+    private fun getIconResource(extension: String): Int {
+        return when (extension.uppercase()) {
+            "MP3", "M4A", "3GA", "AAC", "OGG", "OGA", "WAV", "WMA", "AMR", "AWB", "FLAC", "MID", "MIDI", "XMF", "MXMF", "IMY", "RTTTL", "RTX", "OTA" -> R.drawable.ic_music_64_without_background
+            "GIF", "JPG", "PNG" -> R.drawable.ic_image_64_without_background// не нашёл список всех поддерживаемых планшетом расширений
+            "MP4", "M4V", "3GP", "3G2", "WMV", "ASF", "AVI", "FLV", "MKV", "WEBM" -> R.drawable.ic_video_64_without_background
+            else -> R.drawable.ic_launcher_background
+        }
     }
 }
