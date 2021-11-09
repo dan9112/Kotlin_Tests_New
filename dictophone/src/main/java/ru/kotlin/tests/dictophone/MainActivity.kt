a70751b81fcb9af9b397be4206437e43c14754e6
dan9112_kamaz <danbes1997@gmail.com>
2021-11-09 15:10:59 +0300

    dictaphone v. 1.1
    Оптимизирован код модуля

--- dictophone/src/main/java/ru/kotlin/tests/dictophone/MainActivity.kt
+++ dictophone/src/main/java/ru/kotlin/tests/dictophone/MainActivity.kt
@@ -2,12 +2,12 @@ package ru.kotlin.tests.dictophone
 
 import android.content.Intent
 import android.os.Bundle
-import android.provider.MediaStore
+import android.provider.MediaStore.Audio.Media.RECORD_SOUND_ACTION
 import android.widget.Button
-import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
+import android.widget.Toast.LENGTH_LONG
+import android.widget.Toast.makeText
+import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
 import androidx.appcompat.app.AppCompatActivity
-
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,18 +15,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         findViewById<Button>(R.id.buttonRecord).setOnClickListener {
-            getContent.launch(Intent(MediaStore.Audio.Media.RECORD_SOUND_ACTION))
+            getContent.launch(Intent(RECORD_SOUND_ACTION))
         }
     }
 
-    private val getContent =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            result.data?.let {
-                val savedUri = it.data!!
-                Toast.makeText(
-                    this,
-                    "Saved: " + savedUri.path, Toast.LENGTH_LONG
-                ).show()
-            }
+    private val getContent = registerForActivityResult(StartActivityForResult()) { result ->
+        result.data?.let {
+            makeText(this, "Saved: ${it.data!!.path}", LENGTH_LONG).show()
         }
+    }
 }
