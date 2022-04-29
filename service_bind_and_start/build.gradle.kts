fb8d9b65f477097cadf7bed3198f7f44539341d7
dan9112_kamaz <danbes1997@gmail.com>
2022-04-29 13:53:47 +0300

    service_bind_and_start 1.1
    1. Добавлен связанный сервис

--- service_bind_and_start/build.gradle.kts
+++ service_bind_and_start/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "ru.kamaz.service_bind_and_start"
         minSdk = 24
         targetSdk = 32
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- service_bind_and_start/src/main/java/ru/kamaz/service_bind_and_start/fragments/BlankFragment.kt
+++ service_bind_and_start/src/main/java/ru/kamaz/service_bind_and_start/fragments/BlankFragment.kt
@@ -1,12 +1,19 @@
 package ru.kamaz.service_bind_and_start.fragments
 
+import android.content.ComponentName
+import android.content.Context
+import android.content.Context.BIND_AUTO_CREATE
+import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.IBinder
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import ru.kamaz.service_bind_and_start.R
 import ru.kamaz.service_bind_and_start.databinding.FragmentBlankBinding
+import ru.kamaz.service_bind_and_start.services.MyService
 
 /*private const val EXAMPLE_PARAM = "param"*/
 
@@ -18,11 +25,26 @@ import ru.kamaz.service_bind_and_start.databinding.FragmentBlankBinding
 class BlankFragment : Fragment() {
     /*private var exampleParam: String? = null*/
     private var _binding: FragmentBlankBinding? = null
+    private lateinit var serviceConnection: ServiceConnection
+    private var myService: MyService? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             /*exampleParam = it.getString(EXAMPLE_PARAM)*/
+        }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        serviceConnection = object : ServiceConnection {
+            override fun onServiceConnected(name: ComponentName, binder: IBinder) {
+                myService = (binder as MyService.MyBinder).service
+            }
+
+            override fun onServiceDisconnected(name: ComponentName) {
+                myService = null
+            }
         }
     }
 
@@ -33,6 +55,20 @@ class BlankFragment : Fragment() {
     ).run {
         _binding = this
         root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        with(requireContext()) {
+            Intent(this, MyService::class.java).also { intent ->
+                bindService(intent, serviceConnection, BIND_AUTO_CREATE)
+            }
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        requireContext().unbindService(serviceConnection)
     }
 
     override fun onDestroyView() {
