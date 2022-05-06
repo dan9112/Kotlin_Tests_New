4008cdf6e233b96b5ca28bc3d2c2b18b51063589
dan9112_kamaz <danbes1997@gmail.com>
2022-05-06 10:11:30 +0300

    service_bind_and_start 2.2
    1. Заменён цвет бара навигации на полупрозрачный

--- service_bind_and_start/build.gradle.kts
+++ service_bind_and_start/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "ru.kamaz.service_bind_and_start"
         minSdk = 24
         targetSdk = 32
-        versionCode = 4
-        versionName = "2.1"
+        versionCode = 5
+        versionName = "2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
