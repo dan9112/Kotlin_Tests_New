c93b3abe5cade9467739cc4cccab8d001856a907
dan9112_kamaz <danbes1997@gmail.com>
2022-08-10 09:16:41 +0300

    compose 2.0
    Добавлен пример заполнения псевдосписка с использованием
    декларативной разметки

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,9 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath ("com.android.tools.build:gradle:7.2.1")
-        classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.4.2")
+        classpath(dependencyNotation = "com.android.tools.build:gradle:7.2.1")
+        classpath(dependencyNotation = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.0")
+        classpath(dependencyNotation = "androidx.navigation:navigation-safe-args-gradle-plugin:2.5.1")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
---
+++ jetpack_compose/src/main/java/ru/kamaz/jetpack_compose/MainActivity.kt
@@ -0,0 +1,71 @@
+package ru.kamaz.jetpack_compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Column {
+                for (i in 0..4) {
+                    ListItem(name = "Android", description = "Operating System")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ListItem(name: String, description: String) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(all = 10.dp),
+        shape = RoundedCornerShape(size = 15.dp),
+        elevation = 5.dp
+    ) {
+        Box {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Box {
+                    val modifier = Modifier
+                        .padding(all = 5.dp)
+                        .size(size = 64.dp)
+                        .clip(shape = CircleShape)
+                    val contentScale = ContentScale.Crop
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_launcher_background),
+                        contentDescription = "background",
+                        contentScale = contentScale,
+                        modifier = modifier
+                    )
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                        contentDescription = "icon",
+                        contentScale = contentScale,
+                        modifier = modifier
+                    )
+                }
+                Column(modifier = Modifier.padding(start = 16.dp)) {
+                    Text(text = name)
+                    Text(text = description)
+                }
+            }
+        }
+    }
+}
