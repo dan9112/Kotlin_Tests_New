f12d9a891c3bd8650416a80f493b7b929378c62b
Danil Bessonov <danbes1997@gmail.com>
2022-09-29 09:56:47 +0300

    пример обновлён
    1. Добавлен временной интервал между отправляемыми сообщениями
    2. Добавлена возможность отписки наблюдателя

--- rxjava_3/build.gradle.kts
+++ rxjava_3/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "ru.kamaz.rxjava_3"
         minSdk = 23
         targetSdk = 33
-        versionCode = 5
-        versionName = "2.1"
+        versionCode = 6
+        versionName = "2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- rxjava_3/src/main/java/ru/kamaz/rxjava_3/MainActivity.kt
+++ rxjava_3/src/main/java/ru/kamaz/rxjava_3/MainActivity.kt
@@ -4,27 +4,54 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil.setContentView
 import com.jakewharton.rxbinding.view.RxView.clicks
+import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Observable.just
 import io.reactivex.rxjava3.core.Observable.range
+import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.kotlin.subscribeBy
 import ru.kamaz.rxjava_3.databinding.ActivityMainBinding
-import timber.log.Timber
+import timber.log.Timber.Forest.i
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
+    private val disposable = CompositeDisposable()
+    private val zipper: Observable<String>
+    private var num = 0u
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         with(receiver = setContentView<ActivityMainBinding>(this, R.layout.activity_main)) {
-            // Start the stream when the button is clicked
-            clicks(button).subscribe { startRStream() }
+            clicks(buttonStart).subscribe {
+                startRStream(++num)
+            }
+            clicks(buttonDispose).subscribe {
+                with(disposable) {
+                    if (size() != 0) {
+                        clear()
+                        i("All of active subscribers have been disposed")
+                    } else {
+                        i("There are no active subscribers to dispose")
+                    }
+                }
+            }
         }
     }
 
-    private fun startRStream() {
+    private fun startRStream(num: UInt) {
+        disposable.add(
+            zipper.zipWith(Observable.interval(1, TimeUnit.SECONDS)) { data, _ ->
+                i("$data [$num]")
+                data
+            }.subscribeBy(
+                onNext = { i("$it [$num]") },
+                onComplete = { i("It has been completed [$num]") }
+            )
+        )
+    }
+
+    init {
         val numbers = range(1, 5)
         val strings = just("One", "Two", "Three", "Four", "Five")
-        val zipper = strings.zipWith(numbers) { s, n -> "$s $n" }
-        zipper.subscribeBy(
-            onNext = { Timber.i(it) },
-            onComplete = { Timber.i("It has been completed") })
+        zipper = strings.zipWith(numbers) { s, n -> "$s $n" }
     }
 }
