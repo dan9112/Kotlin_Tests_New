1129ac123d0fdd60fa6e9e4a6333ba63d1f018bc
Danil Bessonov <danbes1997@gmail.com>
2025-02-06 09:22:30 +0300

    добавил заглушку авторизации

--- app/src/main/kotlin/lord/markus/app/MainActivity.kt
+++ app/src/main/kotlin/lord/markus/app/MainActivity.kt
@@ -2,6 +2,7 @@ package lord.markus.app
 
 import android.os.Bundle
 import android.os.Parcelable
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -16,6 +17,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -24,12 +26,15 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowForward
 import androidx.compose.material.icons.automirrored.filled.Send
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
@@ -37,7 +42,6 @@ import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -54,9 +58,11 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import kotlinx.datetime.Instant
@@ -97,11 +103,32 @@ class MainActivity : AppCompatActivity() {
                         modifier = Modifier.fillMaxSize(),
                         containerColor = Color.Transparent
                     ) { innerPadding ->
-                        ChatInterface(
+
+                        var myId by rememberSaveable { mutableStateOf<Long?>(value = null) }
+                        myId?.let { currentId ->
+                            var backTriggered by rememberSaveable { mutableStateOf(value = false) }
+
+                            BackHandler {
+                                backTriggered = !backTriggered
+                            }
+
+                            ChatInterface(
+                                myId = currentId,
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .padding(innerPadding)
+                            )
+
+                            if (backTriggered) LogOutDialog(
+                                currentId = currentId,
+                                resetDialog = { backTriggered = false },
+                                logOut = { myId = null }
+                            )
+                        } ?: AuthInterface(
                             modifier = Modifier
                                 .fillMaxSize()
                                 .padding(innerPadding)
-                        )
+                        ) { myId = it }
                     }
                 }
             }
@@ -110,10 +137,141 @@ class MainActivity : AppCompatActivity() {
 }
 
 @Composable
+private fun LogOutDialog(currentId: Long, resetDialog: () -> Unit, logOut: () -> Unit) =
+    Dialog(onDismissRequest = resetDialog) {
+        Column(
+            modifier = Modifier
+                .background(
+                    color = MaterialTheme.colorScheme.primaryContainer,
+                    shape = RoundedCornerShape(8.dp)
+                )
+                .padding(all = 8.dp),
+            verticalArrangement = Arrangement.spacedBy(4.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Log Out",
+                color = MaterialTheme.colorScheme.onPrimaryContainer,
+                style = MaterialTheme.typography.titleMedium
+            )
+            Text(
+                text = "Are you sure that you want log out, user $currentId?",
+                color = MaterialTheme.colorScheme.onPrimaryContainer,
+                style = MaterialTheme.typography.bodyMedium
+            )
+            Row(
+                horizontalArrangement = Arrangement.SpaceAround,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                TextButton(onClick = resetDialog) {
+                    Text(
+                        text = "Cancel",
+                        color = MaterialTheme.colorScheme.onPrimaryContainer,
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                }
+                Spacer(modifier = Modifier.width(8.dp))
+                TextButton(onClick = logOut) {
+                    Text(
+                        text = "Continue",
+                        color = MaterialTheme.colorScheme.onPrimaryContainer,
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                }
+            }
+        }
+    }
+
+@Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
         modifier = modifier
+    )
+}
+
+@Composable
+inline fun AuthInterface(modifier: Modifier = Modifier, crossinline logIn: (Long) -> Unit) = Column(
+    modifier = modifier,
+    verticalArrangement = Arrangement.spacedBy(4.dp, Alignment.CenterVertically),
+    horizontalAlignment = Alignment.CenterHorizontally
+) {
+    var minus by rememberSaveable { mutableStateOf(false) }
+    var currentId by rememberSaveable { mutableStateOf<Long?>(value = null) }
+    var errorMessage by rememberSaveable { mutableStateOf<String?>(value = null) }
+
+    Text(
+        text = "Log In",
+        color = MaterialTheme.colorScheme.primary,
+        style = MaterialTheme.typography.titleMedium
+    )
+    OutlinedTextField(
+        value = currentId?.toString() ?: if (minus) "-" else "",
+        onValueChange = {
+            if (it.matches(regex = Regex(pattern = "^-?\\d{1,9}$"))) {
+                errorMessage = null
+                currentId = it.toLong()
+                minus = false
+            } else if (it.isEmpty()) {
+                errorMessage = null
+                currentId = null
+                minus = false
+            } else if (it == "-") {
+                errorMessage = null
+                currentId = null
+                minus = true
+            }
+        },
+        textStyle = MaterialTheme.typography.bodyMedium,
+        label = {
+            Text(
+                text = "Your id",
+                style = MaterialTheme.typography.labelMedium
+            )
+        },
+        trailingIcon = {
+            IconButton(
+                onClick = {
+                    currentId
+                        ?.let(logIn)
+                        ?: run { errorMessage = "No id!" }
+                }
+            ) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowForward,
+                    contentDescription = "Log in button icon"
+                )
+            }
+        },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Number,
+            imeAction = ImeAction.Done
+        ),
+        keyboardActions = KeyboardActions(
+            onDone = {
+                currentId
+                    ?.let(logIn)
+                    ?: run { errorMessage = "No id!" }
+            }
+        ),
+        shape = RoundedCornerShape(size = 8.dp),
+        colors = TextFieldDefaults.colors(
+            focusedTextColor = MaterialTheme.colorScheme.primary,
+            unfocusedTextColor = MaterialTheme.colorScheme.primary,
+            disabledTextColor = MaterialTheme.colorScheme.primary.copy(alpha = 0.8f),
+            focusedContainerColor = MaterialTheme.colorScheme.background.copy(alpha = 0.8f),
+            unfocusedContainerColor = MaterialTheme.colorScheme.background.copy(alpha = 0.8f),
+            disabledContainerColor = MaterialTheme.colorScheme.background.copy(alpha = 0.8f),
+            focusedLabelColor = MaterialTheme.colorScheme.primary,
+            unfocusedLabelColor = MaterialTheme.colorScheme.primary,
+            disabledLabelColor = MaterialTheme.colorScheme.primary.copy(alpha = 0.8f)
+        )
+    )
+    Text(
+        text = errorMessage ?: "",
+        color = MaterialTheme.colorScheme.error,
+        style = MaterialTheme.typography.labelMedium
     )
 }
 
@@ -151,11 +309,10 @@ sealed interface MessageState : Parcelable {
 @OptIn(FormatStringsInDatetimeFormats::class)
 @Composable
 fun ChatInterface(
+    myId: Long,
     modifier: Modifier = Modifier,
     coroutineScope: CoroutineScope = rememberCoroutineScope()
 ) {
-    val myId by rememberSaveable { mutableLongStateOf(value = -1L) }
-
     val messages = rememberSaveable(
         saver = listSaver(save = { it.toList() }, restore = { it.toMutableStateList() })
     ) { mutableStateListOf<Message>() }
