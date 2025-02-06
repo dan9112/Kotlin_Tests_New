99545a01eee531c60891c0dbdb6107207675c9ec
Danil Bessonov <danbes1997@gmail.com>
2025-02-06 09:24:03 +0300

    исправил поведение при открытии клавиатуры

--- app/src/main/kotlin/lord/markus/app/MainActivity.kt
+++ app/src/main/kotlin/lord/markus/app/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
@@ -90,6 +91,7 @@ class MainActivity : AppCompatActivity() {
                     modifier = Modifier
                         .fillMaxSize()
                         .background(color = MaterialTheme.colorScheme.background)
+                        .imePadding()
                 ) {
                     Image(
                         painter = painterResource(id = R.drawable.ic_launcher_foreground),
